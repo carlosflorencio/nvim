9b1f649e226b93d8d4b48cb0ea053f3194594487
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-03-15 23:03:04 +0000

    feat: disable treesitter for large files

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -36,6 +36,13 @@ return {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
+
+        -- disable slow treesitter highlight for large files
+        disable = function(_, buf)
+          local max_filesize = 500 * 1024 -- 500 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then return true end
+        end,
       },
       -- nvim-ts-autotag
       autotag = {
