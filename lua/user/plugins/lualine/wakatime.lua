25ce62ca19805295796d8d8cce2a12a5d5822a1e
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-03-22 20:18:49 +0000

    feat: change wakatime bin

--- lua/user/plugins/lualine/wakatime.lua
+++ lua/user/plugins/lualine/wakatime.lua
@@ -5,7 +5,9 @@ local utils = require "user.util"
 local current_time = ""
 local function set_interval(interval, callback)
   local timer = uv.new_timer()
-  local function ontimeout() callback(timer) end
+  local function ontimeout()
+    callback(timer)
+  end
 
   uv.timer_start(timer, interval, interval, ontimeout)
   return timer
@@ -16,7 +18,7 @@ local function update_wakatime()
   local stdout = uv.new_pipe()
   local stderr = uv.new_pipe()
 
-  local _, _ = uv.spawn("/users/carlosflorencio/Library/Python/3.9/bin/wakatime", {
+  local _, _ = uv.spawn("/opt/homebrew/bin/wakatime-cli", {
     args = { "--today" },
     stdio = { stdin, stdout, stderr },
   }, function() -- on exit
@@ -31,13 +33,19 @@ local function update_wakatime()
       local parsed = utils.trim(data)
       ---@diagnostic disable-next-line: missing-parameter
       local parts = utils.split(parsed)
-      if parts[1] ~= nil and parts[3] ~= nil then current_time = " " .. parts[1] .. "h" .. parts[3] .. "m" end
+      if parts[1] ~= nil and parts[3] ~= nil then
+        current_time = " " .. parts[1] .. "h" .. parts[3] .. "m"
+      end
     end
   end)
 end
 
 set_interval(90000, update_wakatime)
 
-vim.fn.timer_start(2000, function() update_wakatime() end)
+vim.fn.timer_start(2000, function()
+  update_wakatime()
+end)
 
-function Lualine_get_wakatime() return current_time end
+function Lualine_get_wakatime()
+  return current_time
+end
