79a5a59633097eea24725423914e71eeaee0e416
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-06-17 19:16:38 +0100

    feat: keep cursorline on nvimtree

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -258,12 +258,18 @@ return {
   },
 
   {
+    -- cursorline only for the active window
     "tummetott/reticle.nvim",
     enabled = true,
     -- lazy = false,
     event = "VeryLazy", -- lazyload the plugin if you like
     opts = {
       -- add options here if you want to overwrite defaults
+      ignore = {
+        cursorline = {
+          "NvimTree",
+        },
+      },
     },
   },
 }
