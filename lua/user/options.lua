4fd429b4bc2f8216c6be887a261a0257016eca1b
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-10-16 23:11:00 +0100

    add tab settings for golang

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -45,3 +45,10 @@ vim.o.breakindent = true
 vim.o.showtabline = 2
 
 -- vim.g.editorconfig = false
+--
+
+-- make tabs use 4 spaces
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
