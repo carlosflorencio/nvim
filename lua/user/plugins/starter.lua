fd8424c869045b41fe35f036e08042cecf99da7f
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-11-26 19:27:33 +0000

    update plugins

--- lua/user/plugins/starter.lua
+++ lua/user/plugins/starter.lua
@@ -11,19 +11,17 @@ return {
           },
           shortcut = {
             {
-              desc = " Update",
-              group = "@property",
-              action = "Lazy update",
-              key = "u",
-            },
-            {
-              icon = " ",
-              icon_hl = "@variable",
-              desc = "Files",
+              desc = "󰱼 Files",
               group = "Label",
               action = "lua require('telescope').extensions.smart_open.smart_open()",
               key = "f",
             },
+          },
+          project = {
+            enable = false,
+          },
+          mru = {
+            cwd_only = true,
           },
         },
       }
