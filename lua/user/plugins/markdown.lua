a5f9163a9788b2bfad82967f49193c08a869fecc
Carlos Florêncio <carlosmflorencio@gmail.com>
2024-05-01 16:38:54 +0100

    update plugins

--- lua/user/plugins/markdown.lua
+++ lua/user/plugins/markdown.lua
@@ -17,7 +17,7 @@ return {
     'mrjones2014/mdpreview.nvim',
     lazy = true,
     -- ft = 'markdown', -- you can lazy load on markdown files only
-    -- requires the `terminal` filetype to render ASCII color and format codes
+    -- requires the `terminalk filetype to render ASCII color and format codes
     dependencies = { 'norcalli/nvim-terminal.lua', config = true },
     config = function()
       require('mdpreview').setup {}
