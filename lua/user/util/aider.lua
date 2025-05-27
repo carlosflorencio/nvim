5161b16b3adb2fd92b1f05b4b61a8438afe8d103
Carlos Florêncio <carlosmflorencio@gmail.com>
2025-05-27 23:27:03 +0100

    feat: Add Aider keymaps for drop/errors, update toggle, set width

--- lua/user/util/aider.lua
+++ lua/user/util/aider.lua
@@ -14,6 +14,12 @@ M.setup = function()
     direction = 'vertical',
     on_open = function(term)
       vim.cmd 'startinsert!'
+
+      -- adjust width
+      local width = math.floor(vim.o.columns * 0.4)
+      local winid = vim.api.nvim_get_current_win()
+      vim.api.nvim_win_set_width(winid, width)
+
       vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
       vim.api.nvim_buf_set_keymap(term.bufnr, 'n', '<esc>', '<cmd>close<CR>', { noremap = true, silent = true })
     end,
@@ -30,18 +36,35 @@ M.setup = function()
     desc = 'Aider local dir',
   })
 
-  vim.keymap.set('n', '<leader>a', function()
+  -- toggle aider
+  vim.keymap.set('n', '<leader>at', function()
     aider:toggle()
   end, { noremap = true, silent = true })
 
-  vim.keymap.set('v', '<leader>a', function()
-    local trim_spaces = true
-    require('toggleterm').send_lines_to_terminal('visual_lines', trim_spaces, { args = 1 })
-  end, { noremap = true, silent = true })
-
+  -- add file
   vim.keymap.set('n', '<leader>aa', function()
     local path = vim.fn.expand '%:p'
     aider:send('/add ' .. path)
+  end, { noremap = true, silent = true })
+
+  -- drop file
+  vim.keymap.set('n', '<leader>ad', function()
+    local path = vim.fn.expand '%:p'
+    aider:send('/drop ' .. path)
+  end, { noremap = true, silent = true })
+
+  vim.keymap.set('n', '<leader>ae', function()
+    local errors = require('user.util.lsp').get_lsp_errors()
+
+    vim.fn.setreg('*', 'Fix these LSP Errors:\n' .. errors)
+
+    aider:send '/paste'
+  end, { noremap = true, silent = true })
+
+  -- visual mode
+  vim.keymap.set('v', '<leader>a', function()
+    local trim_spaces = true
+    require('toggleterm').send_lines_to_terminal('visual_lines', trim_spaces, { args = 1 })
   end, { noremap = true, silent = true })
 end
 
