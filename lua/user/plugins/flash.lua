2573bb98d1a3c8d063b5c5cf7f281bf8a0d15c1d
Carlos Florêncio <carlosmflorencio@gmail.com>
2025-06-06 16:51:44 +0100

    Updated files 2025-06-06 16:51:44

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -18,6 +18,7 @@ return {
         },
         char = {
           enabled = false,
+          -- jump_labels = true,
           keys = { 'f', 'F', ';', ',' },
           label = { exclude = 'hjkliardco' },
         },
@@ -25,12 +26,12 @@ return {
     },
 
     -- stylua: ignore
-        keys = {
-                { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-                { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-                { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-                { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-                { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-        },
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
   },
 }
