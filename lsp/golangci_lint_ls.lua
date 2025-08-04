8b5c4d0240008099b8bc1c5db229b7874e481886
Carlos Florêncio <carlosmflorencio@gmail.com>
2025-08-04 12:10:52 +0100

    Updated files 2025-08-04 12:10:52

--- lsp/golangci_lint_ls.lua
+++ lsp/golangci_lint_ls.lua
@@ -1,5 +1,5 @@
 ---@type vim.lsp.Config
 return {
   root_markers = { '.test' },
-  root_dir = vim.lsp.util.root_pattern('go.mod', '.git'),
+  root_dir = require('lspconfig').util.root_pattern('deno.json', 'deno.jsonc'),
 }
