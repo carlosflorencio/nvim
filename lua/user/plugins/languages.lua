1df4e5b106e7d559e2f8c8e451d77dd3967d6fc2
Carlos Florêncio <carlosmflorencio@gmail.com>
2025-10-06 14:13:27 +0100

    Updated files 2025-10-06 14:13:27

--- lua/user/plugins/languages.lua
+++ lua/user/plugins/languages.lua
@@ -7,10 +7,6 @@ return {
     opts = {
       hide_up_to_date = true,
       hide_unstable_versions = true,
-      colors = {
-        -- up_to_date = '#3C4048', -- Text color for up to date dependency virtual text
-        -- outdated = '#d19a66', -- Text color for outdated dependency virtual text
-      },
     },
     keys = {
       {
