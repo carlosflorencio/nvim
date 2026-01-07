c4eda739a5b082d272503988b270dae1a57c4df1
Carlos Florêncio <carlosmflorencio@gmail.com>
2026-01-07 22:36:49 +0000

    Updated files 2026-01-07 22:36:49

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -8,6 +8,11 @@ return {
       completion = {
         callSnippet = 'Replace',
       },
+      workspace = {
+        library = {
+          -- ['/Applications/Hammerspoon.app/Contents/Resources/extensions/hs/'] = true,
+        },
+      },
       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
     },
